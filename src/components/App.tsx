608ebbbadc48a88f59d6b7dc4b3490f08b64119f
rulewrite <rulewrite@gmail.com>
2022-08-11 00:30:05 +0900

    feat: react-router-dom 추가

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      hello App
+      <br />
+      <Link to="/b">Go to B</Link>
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import App from './components/App';
+import Bpage from './components/Bpage';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    header
+    <BrowserRouter>
+      <Routes>
+        <Route path="/b" element={<Bpage />}></Route>
+        <Route path="/" element={<App />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
