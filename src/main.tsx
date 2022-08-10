607a2ab897a2c658a9c3b823c4aa5438c6da6c10
rulewrite <rulewrite@gmail.com>
2022-08-11 01:53:47 +0900

    chore: 미사용 css 파일 제거

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
-import './index.css';
 import Mui from './Mui';
 import Routes from './Routes';
 
