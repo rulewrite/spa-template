11318417de088e5e5c5c75118979cf822306a338
rulewrite <rulewrite@gmail.com>
2022-08-18 01:29:41 +0900

    feat: recoil 상태관리 라이브러리 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,20 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 import Header from './Header';
 import Routes from './Routes';
 import theme from './theme';
 
 export default () => {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <Header />
-        <Routes />
-      </BrowserRouter>
-    </ThemeProvider>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <Header />
+          <Routes />
+        </BrowserRouter>
+      </ThemeProvider>
+    </RecoilRoot>
   );
 };
